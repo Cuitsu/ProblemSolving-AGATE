3b1c9812912dfd404e19863b0480952b67a34690
Cuitsu <bagassurya25@gmail.com>
2021-10-14 00:50:41 +0700

    prob2

---
+++ Assets/Scripts/BallMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    //Rigidbody 2d bola
+    private Rigidbody2D rb;
+
+    //Besarnya gaya
+    public float xInitialForce;
+    public float yInitialForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        PushBall();
+    }
+
+    //Dorong bola
+    void PushBall()
+    {
+        rb.AddForce(new Vector2(xInitialForce, yInitialForce));
+    }
+}
