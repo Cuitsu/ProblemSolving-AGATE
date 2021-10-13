f475b1bda082c73f4a9b36cafb1e86f8261bd6aa
Cuitsu <bagassurya25@gmail.com>
2021-10-14 02:27:45 +0700

    prob7

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private Text scorePoint;
+    public static int score;
+
+    private void Start()
+    {
+        score = 0;
+        scorePoint = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        scorePoint.text = score.ToString();
+    }
+}
