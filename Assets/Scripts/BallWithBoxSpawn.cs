1edff3c5ab27001fc6e8d6638257651309d025be
Cuitsu <bagassurya25@gmail.com>
2021-10-15 16:48:35 +0700

    main game

---
+++ Assets/Scripts/BallWithBoxSpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallWithBoxSpawn : MonoBehaviour
+{
+    //Rigidbody 2d bola
+    private Rigidbody2D rb;
+
+    //Besarnya gaya
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    //Gerakan bola
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Box")
+        {
+            Destroy(collision.gameObject);
+            Score.score += 1;
+        }
+    }
+}
---
+++ Assets/Scripts/KeyboardMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardMove : MonoBehaviour
+{
+    //Rigidbody 2d bola
+    private Rigidbody2D rb;
+
+    //Besarnya gaya
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    //Gerakan bola
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(speed * horizontal, speed * vertical);
+    }
+}
